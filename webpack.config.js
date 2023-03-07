db7e8735fbe9ae41f12e24cf815bec60bf2ec25c
GHOST-Aram <owinoojala@gmail.com>
2023-03-07 14:44:47 +0300

    Install and configure webpack-dev-server, html-webpack-plugin, css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path")
 module.exports = {
     mode: 'development',
@@ -8,5 +9,32 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
         clean: true,
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Plan Day',
+            template: './src/templates/index.html',
+            filename: 'index.html'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname,'./dist'),
+        },
+        port: 3000,
+        open: true,
+        compress: true,
+        hot: true,
+        historyApiFallback: true,
+
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+
 }
