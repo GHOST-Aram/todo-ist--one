8c9093dadc9775c7bbdfcc009e2285f11ed7bb7b
GHOST-Aram <owinoojala@gmail.com>
2023-03-07 18:04:33 +0300

    feat: Create ProjectManager Class
         -ProjectManager handles project data

---
+++ src/scripts/project_manager.js
@@ -0,0 +1,36 @@
+export default class ProjectManager {
+     /*
+    Project manager class performs operations on Projects
+    */
+    #projects = []
+    #completedProjects = []
+    constructor () {
+
+    }
+    //Add to list
+    addProject(project){
+        this.#projects.push(project)
+    }
+    //Update completed projects
+    addToCompleted(project){
+        this.#completedProjects.push(project)
+    }
+    //Get list of completed projects
+    getCompleted(){
+        return this.#completedProjects
+    }
+    //get projects
+    getProjects(){
+        return this.#projects
+    }
+    //remove project from list
+    removeProject(project){
+        this.#projects = this.#projects.filter(element => {
+            if(element === project)
+                return false
+            else
+                return true
+        })
+    }
+
+}
