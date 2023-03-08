30beae53c36e76bf778d60f9705b1689f45ae42a
GHOST-Aram <owinoojala@gmail.com>
2023-03-08 18:44:14 +0300

    feat:Create and display sidebar

--- src/scripts/dom_manager.js
+++ src/scripts/dom_manager.js
@@ -2,23 +2,34 @@ export default class DOMManager {
     constructor () {
 
     }
-   createHeader(){
-        const header = document.createElement('header')
-        header.className = 'p-4 bg-blue-700'
-        return header
+   createHeader () {
+     const header = document.createElement('header')
+     header.className = 'p-4 bg-blue-700 m-auto mx-4 rounded-top rounded-md'
+     return header
    }
-   createHeading(text){
-        const heading = document.createElement('h1')
-        heading.className = 'text-xl text-slate-200'
-        heading.textContent = text
+   createHeading (text) {
+     const heading = document.createElement('h1')
+     heading.className = 'text-xl text-slate-200'
+     heading.textContent = text
 
-        return heading
+     return heading
    }
-   createLogo(text){
-        const logo = this.createHeading(text)
-        logo.classList.remove('text-xl')
-        logo.classList.add('text-2xl','font-bold','text-center')
+   createLogo (text) {
+     const logo = this.createHeading(text)
+     logo.classList.remove('text-xl')
+     logo.classList.add('text-2xl','font-bold','text-center')
 
-        return logo
+     return logo
    }
+   createSidebar () {
+     const sidebar = document.createElement('aside')
+     sidebar.className = 'p-4 bg-blue-700 rounded-md'
+     
+     return sidebar
+   }
+   render (content) {
+     const contentContainer = document.querySelector('#content')
+     contentContainer.appendChild(content)
+   }
+   
 }
