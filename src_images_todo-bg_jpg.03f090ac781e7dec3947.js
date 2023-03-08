4958e1f877bf7300ba5b259c7b332e272dd630d1
GHOST-Aram <owinoojala@gmail.com>
2023-03-08 10:34:31 +0300

    feat: Set body background image

---
+++ src_images_todo-bg_jpg.03f090ac781e7dec3947.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunktodo_ist_one"] = self["webpackChunktodo_ist_one"] || []).push([["src_images_todo-bg_jpg"],{
+
+/***/ "./src/images/todo-bg.jpg":
+/*!********************************!*\
+  !*** ./src/images/todo-bg.jpg ***!
+  \********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "todo-bg.jpg";
+
+/***/ })
+
+}]);
