44ebca5c01eca138db2d68c6233703bf2d5261dc
GHOST-Aram <owinoojala@gmail.com>
2023-03-08 06:09:01 +0300

    feat: Add project open property to project
         -include setters and accessor methods for open status

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ One Page App todo list
 # Task Class
     create task onject
     
+# DOMManager
+    sidebar:
+        profile pic and name
+
+        default project folder: open or closed
+        project folder: open or closed
+
+        
+
+        footer
--- src/scripts/project.js
+++ src/scripts/project.js
@@ -1,9 +1,25 @@
 import TaskManager from "./task_manager";
 
 export default class Project extends TaskManager {
+
+    //Project open
+    #projectOpen = false
     constructor(name){
         super()
         this.name = name
     }
+    //close project
+    close(){
+        this.#projectOpen = false
+    }
+    //Check if project is open
+    isOpen(){
+        return this.#projectOpen
+    }
+    //Open project
+    open(){
+        this.#projectOpen = true
+    }
+
 }
 
